81a5526bb9340886161fa71207706f1b2f2c9581
Nageswara <nageswara.nandimalla@gmail.com>
2024-02-20 10:38:36 -0500

    msg api created

--- SpringBoot1/src/test/java/com/example/demo/contorller/WelcomeController.java
+++ SpringBoot1/src/test/java/com/example/demo/contorller/WelcomeController.java
@@ -1,5 +1,15 @@
 package com.example.demo.contorller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+	@GetMapping("/msg")
+	public String getMsg()
+	{
+		return " Hello World";
+		
+	}
 
 }
